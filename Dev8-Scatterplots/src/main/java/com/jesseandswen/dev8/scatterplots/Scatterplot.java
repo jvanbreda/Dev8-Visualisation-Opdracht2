e52e8f91bfc103573cecf7b3fb7f70583c3a5198
swenmeeuwes <swenmeeu@gmail.com>
2016-05-19 20:10:47 +0200

    Added: Helper lines
    Move over the scatterplot to see them!

--- Dev8-Scatterplots/src/main/java/com/jesseandswen/dev8/scatterplots/Scatterplot.java
+++ Dev8-Scatterplots/src/main/java/com/jesseandswen/dev8/scatterplots/Scatterplot.java
@@ -23,25 +23,27 @@ import static processing.core.PApplet.map;
  * @author swenm_000
  */
 public class ScatterPlot {
+
     private PApplet applet;
-    
+
     private Rect<Integer> area;
     private List<DataModel> dataList;
     private List<DataModel> mappedDataList;
-    
-    private Vector2<Integer> intervals = new Vector2<Integer>(10, 10);
+
+    private Vector2<Float> intervals = new Vector2<Float>(10f, 10f);
 
     public ScatterPlot(PApplet applet, Rect<Integer> area, List<DataModel> data) {
         this.applet = applet;
         this.area = area;
         this.dataList = data;
-        
+
         mappedDataList = mapData(dataList);
     }
-    
+
     public void draw() {
         drawAxis();
-        
+        drawHelpLines();
+
         for (DataModel model : mappedDataList) {
             Rgb rgb = Rgb.RED;
             switch (model.getCAT()) {
@@ -65,80 +67,101 @@ public class ScatterPlot {
             applet.ellipse(model.getEIG1(), model.getEIG2(), 5, 5);
         }
     }
-    
+
     public void intervalEvery(int x, int y) {
         Vector2<Float> maxValues = getRoundedMaxValues();
-        
-        int intervalX = (int)(maxValues.x / (float)x);
-        int intervalY = (int)(maxValues.y / (float)y);
-        
+
+        float intervalX = maxValues.x / x;
+        float intervalY = maxValues.y / y;
+
         intervals = new Vector2<>(intervalX, intervalY);
     }
     
+    private void drawHelpLines() {
+        Vector2<Integer> mousePosition = new Vector2(applet.mouseX, applet.mouseY);
+        Vector2<Float> maxValues = getRoundedMaxValues();
+
+        // If mouse is in area
+        if (mousePosition.x > area.x && mousePosition.x < area.x + area.width && mousePosition.y < area.y && mousePosition.y > area.y - area.height) {
+            applet.stroke(125);
+            
+            applet.textAlign(applet.CENTER, applet.BOTTOM);
+            applet.text((int) map(mousePosition.x, area.x, area.x + area.width, 0, maxValues.x), mousePosition.x, area.y - area.height - 4);
+            
+            applet.textAlign(applet.LEFT, applet.CENTER);
+            applet.text((int) map(mousePosition.y, area.y, area.y - area.height, 0, maxValues.y), area.x + area.width + 4, mousePosition.y);
+            
+            applet.line(area.x, mousePosition.y, area.x + area.width, mousePosition.y); // Horizontal line (X-axis)
+            applet.line(mousePosition.x, area.y, mousePosition.x, area.y - area.height); // Vertical line (Y-axis)
+        }
+    }
+
     private List<DataModel> mapData(List<DataModel> data) {
         Vector2<Float> maxValues = getRoundedMaxValues();
         DataModel[] newData = new DataModel[data.size()];
         for (int i = 0; i < data.size(); i++) {
             DataModel model = new DataModel();
-            model.setCAT(data.get(i).getCAT());  
-            model.setEIG1((int)map(data.get(i).getEIG1(), 0, maxValues.x, area.x, area.x + area.width));
+            model.setCAT(data.get(i).getCAT());
+            model.setEIG1((int) map(data.get(i).getEIG1(), 0, maxValues.x, area.x, area.x + area.width));
             model.setEIG2(map(data.get(i).getEIG2(), 0, maxValues.y, area.y, area.y - area.height));
-            
+
             newData[i] = model;
         }
         return Arrays.asList(newData);
     }
-    
-    // HMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+    // HMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM???????????????????
     private Vector2<Float> getRoundedMaxValues() {
         Vector2<Float> maxValues = getMaxValues();
-        
+
         int lengthOfX = String.valueOf(Math.round(maxValues.x)).length();
         int lengthOfY = String.valueOf(Math.round(maxValues.y)).length();
-        
+
         maxValues.x = maxValues.x - (maxValues.x % 10);
         maxValues.y = maxValues.y - (maxValues.y % 100) + 100;
-        
+
         return maxValues;
     }
-    
+
     private Vector2<Float> getMaxValues() {
         float maxEIG1 = 0f;
         float maxEIG2 = 0f;
-        
-        for(DataModel model : dataList) {
-            if(model.getEIG1() > maxEIG1)
+
+        for (DataModel model : dataList) {
+            if (model.getEIG1() > maxEIG1) {
                 maxEIG1 = model.getEIG1();
-            if(model.getEIG2() > maxEIG2)
+            }
+            if (model.getEIG2() > maxEIG2) {
                 maxEIG2 = model.getEIG2();
+            }
         }
-        
+
         return new Vector2<>(maxEIG1, maxEIG2);
     }
-    
+
     private void drawAxis() {
         applet.stroke(0);
         applet.line(area.x, area.y, area.x + area.width, area.y); // Horizontal line (X-axis)
         applet.line(area.x, area.y, area.x, area.y - area.height); // Vertical line (Y-axis)
-        
+
         Vector2<Float> maxValues = getRoundedMaxValues();
         applet.fill(0);
         int lineHeight = 10;
-        
+
         // Horizontale intervals
         applet.textAlign(applet.CENTER, applet.TOP);
         for (int i = 1; i <= intervals.x; i++) {
             applet.line(area.x + (i * (area.width / intervals.x)), area.y, area.x + (i * (area.width / intervals.x)), area.y + lineHeight);
             applet.text(Math.round((maxValues.x / intervals.x) * i), area.x + (i * (area.width / intervals.x)), area.y + lineHeight + 4);
         }
-        
+
         // Vertical intervals
         applet.textAlign(applet.RIGHT, applet.CENTER);
         for (int i = 1; i <= intervals.y; i++) {
             applet.line(area.x, area.y - (i * (area.height / intervals.y)), area.x - lineHeight, area.y - (i * (area.height / intervals.y)));
             applet.text(Math.round((maxValues.y / intervals.y) * i), area.x - lineHeight - 4, area.y - (i * (area.width / intervals.y)));
         }
-        
+
         // Origin
         applet.textAlign(applet.RIGHT, applet.TOP);
         applet.text(0, area.x - 4, area.y - 4);
